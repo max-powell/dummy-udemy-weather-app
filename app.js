fa6e6f412ed2d0016dbf3f11dc9742a209e8c281
max-powell <github email address>
2019-08-25 11:14:00 +0100

    added separate geocode utils function

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/5fb197eebdeaf31dd8b2052e84c742c9/37.8267,-122.4233'
+const geocode = require('./utils/geocode')
 
-request({url}, (err, res) => {
-  const data = JSON.parse(res.body)
-  console.log(data.currently)
+const darkskyUrl = 'https://api.darksky.net/forecast/5fb197eebdeaf31dd8b2052e84c742c9/37.8267,-122.4233?units=si'
+
+// request({url: darkskyUrl, json: true}, (err, {body}) => {
+//   if (err) {
+//     console.log('Unable to connect to weather service')
+//   } else if (body.error) {
+//     log(body.error)
+//   } else {
+//     console.log(body.daily.data[0].summary)
+//     console.log(`It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability * 100}% chance of rain.`);
+//   }
+// })
+
+geocode('kt22 0dj', (err, data) => {
+  console.log('Error: ', err)
+  console.log('Data: ', data)
 })
